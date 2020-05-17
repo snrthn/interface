84fe9b9f811581f3bdda4d9c69e0876b78f20856
ex_yanghuning <yang.huning@chinamworld.com>
2020-05-18 00:08:23 +0800

    '取目录列表'

---
+++ server.js
@@ -0,0 +1,19 @@
+﻿var http = require('http');
+// var path = require('path');
+// var bodyParser = require('body-parser');
+// var urlParser = require('url');
+var app = require('./src');
+
+
+app(function (obj) {
+	console.log(obj);
+});
+
+
+// var server = http.createServer(function (req, res) {
+
+// });
+
+// server.listen(2306, function () {
+// 	console.log('MU2306');
+// });
---
+++ src/index.js
@@ -0,0 +1,27 @@
+﻿var path = require('path');
+var rd = require('rd');
+
+function readDir (dir) {
+	return new Promise(function (resolve, reject) {
+		rd.read(path.join(__dirname, dir), function (err, files) {
+			if (err) reject(err);
+			resolve(files);
+		});
+	})
+}
+
+module.exports = function (callback) {
+	var dataCollection = [];
+	var contCollection = [];
+	Promise.all([
+		readDir('./data'),
+		readDir('./controller')
+	]).then(function ([arr1, arr2]) {
+		callback({
+			dataCollection: arr1,
+			contCollection: arr2
+		});
+	}).catch(function (err) {
+		console.log(err);
+	});
+}
