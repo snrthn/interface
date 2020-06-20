c01c01725bec27bbbb6385abea5f5fff45a5b5fc
ex_yanghuning <yang.huning@chinamworld.com>
2020-06-20 10:18:31 +0800

    '控制器底层逻辑重写'

--- src/index.js
+++ src/index.js
@@ -39,76 +39,8 @@ function initServer () {
 
 			/* 匹配路由控制表 */
 			routerList.map(function (item) {
-				if (item.data && item.data.substr(0, item.data.lastIndexOf('.')) === urlObj.pathname) {
-					isNoPage = true;
-					postDataFun(req, function (postData) {
-						/* 输出 */
-						var jsonData;
-						if (!item.cont) {
-							fs.readFile(path.join(__dirname, './data') + item.data, function (err, data) {
-								if (err) {
-									res.write('错误: 数据 json 文件读取失败!');
-									res.end();
-								} else {
-									var dataStr = data.toString();
-									try {
-										/* 对 utf-8 编码的 JSON 文件进行特殊处理 */
-										if (dataStr.substr(0, 1).charCodeAt() === 65279) {
-											dataStr = dataStr.substr(1, dataStr.length - 1);
-										}
-										/* 检查文件内容是否符合 JSON 规范 */
-										var json = JSON.parse(dataStr);
 
-										res.write(JSON.stringify(json));
-										res.end();
-									} catch (err) {
-										res.write('错误: 数据 json 内部不是一个有效的JOSN数据!');
-										res.end();
-									}
-								}
-							})
-						} else {
-							jsonData = require(path.join(__dirname, './data') + item.data);
-							fs.readFile(path.join(__dirname, './controller') + item.cont, function (err, data) {
-								if (err) {
-									res.write('错误: 控制器 js 文件读取失败!');
-									res.end();
-								} else {
-									var strFun = data.toString();
-									try {
-										/* 对 utf-8 编码的 JSON 文件进行特殊处理 */
-										if (strFun.substr(0, 1).charCodeAt() === 65279) {
-											strFun = strFun.substr(1, strFun.length - 1);
-										}
-
-										// 控制器为空
-										if (strFun.length === 0) {
-											res.write('错误: 控制器文件为空!');
-											res.end();
-										} else if (strFun.indexOf('.write(') === -1 && strFun.indexOf('.end(') === -1) {
-											res.write('错误: 控制器函数体内缺少回执语句!');
-											res.end();
-										} else {
-											Reflect.deleteProperty(require.cache, require.resolve(path.join(__dirname, './controller') + item.cont));
-											var conFun = require(path.join(__dirname, './controller') + item.cont);
-											if (conFun && typeof conFun === 'function') {
-												conFun(req, res, urlObj.query, postData, jsonData);
-											} else {
-												res.write('错误: 控制器必须是一个函数!');
-												res.end();
-											}
-										}
-
-									} catch (err) {
-										res.write('错误: ' + err.message);
-										res.end();
-									}
-								}
-							})
-						}
-					});
-				}
-				if (!item.data && item.cont && item.cont.substr(0, item.cont.lastIndexOf('.')) === urlObj.pathname) {
+				if (item.data && item.cont && item.data.substr(0, item.data.lastIndexOf('.')) === urlObj.pathname) {
 					isNoPage = true;
 					postDataFun(req, function (postData) {
 						fs.readFile(path.join(__dirname, './controller') + item.cont, function (err, data) {
@@ -117,7 +49,6 @@ function initServer () {
 								res.end();
 							} else {
 								var strFun = data.toString();
-								
 								try {
 									/* 对 utf-8 编码的 JSON 文件进行特殊处理 */
 									if (strFun.substr(0, 1).charCodeAt() === 65279) {
@@ -135,20 +66,84 @@ function initServer () {
 										Reflect.deleteProperty(require.cache, require.resolve(path.join(__dirname, './controller') + item.cont));
 										var conFun = require(path.join(__dirname, './controller') + item.cont);
 										if (conFun && typeof conFun === 'function') {
-											conFun(req, res, urlObj.query, postData, null);
+											conFun(req, res, urlObj.query, postData, jsonData);
 										} else {
 											res.write('错误: 控制器必须是一个函数!');
 											res.end();
 										}
 									}
-
 								} catch (err) {
 									res.write('错误: ' + err.message);
 									res.end();
 								}
 							}
-						})
-					})
+						});
+					});
+				} else if (item.data && !item.cont && item.data.substr(0, item.data.lastIndexOf('.')) === urlObj.pathname) {
+					isNoPage = true;
+					postDataFun(req, function (postData) {
+						fs.readFile(path.join(__dirname, './data') + item.data, function (err, data) {
+							if (err) {
+								res.write('错误: 数据 json 文件读取失败!');
+								res.end();
+							} else {
+								var dataStr = data.toString();
+								try {
+									/* 对 utf-8 编码的 JSON 文件进行特殊处理 */
+									if (dataStr.substr(0, 1).charCodeAt() === 65279) {
+										dataStr = dataStr.substr(1, dataStr.length - 1);
+									}
+									/* 检查文件内容是否符合 JSON 规范 */
+									var json = JSON.parse(dataStr);
+
+									res.write(JSON.stringify(json));
+									res.end();
+								} catch (err) {
+									res.write('错误: 数据 json 内部不是一个有效的JOSN数据!');
+									res.end();
+								}
+							}
+						});
+					});
+				} else if (!item.data && item.cont && item.cont.substr(0, item.cont.lastIndexOf('.')) === urlObj.pathname) {
+					isNoPage = true;
+					postDataFun(req, function (postData) {
+						fs.readFile(path.join(__dirname, './controller') + item.cont, function (err, data) {
+							if (err) {
+								res.write('错误: 控制器 js 文件读取失败!');
+								res.end();
+							} else {
+								var strFun = data.toString();
+								try {
+									/* 对 utf-8 编码的 JSON 文件进行特殊处理 */
+									if (strFun.substr(0, 1).charCodeAt() === 65279) {
+										strFun = strFun.substr(1, strFun.length - 1);
+									}
+
+									// 控制器为空
+									if (strFun.length === 0) {
+										res.write('错误: 控制器文件为空!');
+										res.end();
+									} else if (strFun.indexOf('.write(') === -1 && strFun.indexOf('.end(') === -1) {
+										res.write('错误: 控制器函数体内缺少回执语句!');
+										res.end();
+									} else {
+										Reflect.deleteProperty(require.cache, require.resolve(path.join(__dirname, './controller') + item.cont));
+										var conFun = require(path.join(__dirname, './controller') + item.cont);
+										if (conFun && typeof conFun === 'function') {
+											conFun(req, res, urlObj.query, postData, jsonData);
+										} else {
+											res.write('错误: 控制器必须是一个函数!');
+											res.end();
+										}
+									}
+								} catch (err) {
+									res.write('错误: ' + err.message);
+									res.end();
+								}
+							}
+						});
+					});
 				}
 			})
 
@@ -176,20 +171,18 @@ function fillRouterTable (callback) {
 
 		var temp = [];
 		var datExt = '';
-		var conExt = '';
 
 		for (var i = 0; i < dataLen; i++) {
 			var data = dataCollection[i];
-			if (!datExt) datExt = data.split(data.lastIndexOf('.'), data.length - 1);
+			if (!datExt) datExt = data.substr(data.lastIndexOf('.'), data.length - 1);
 			var resDat = data.substr(0, data.lastIndexOf('.'));
 			temp.push(resDat);
 		}
 
 		for (var i = 0; i < contLen; i++) {
 			var cont = contCollection[i];
-			if (!conExt) conExt = cont.split(cont.lastIndexOf('.'), cont.length - 1);
 			var resCon = cont.substr(0, cont.lastIndexOf('.'));
-			if (temp.indexOf(resCon) !== -1) {				
+			if (temp.indexOf(resCon) !== -1) {
 				routerList.push({
 					data: resCon + datExt,
 					cont
@@ -207,7 +200,7 @@ function fillRouterTable (callback) {
 
 		for (var i = 0; i < tempLen; i++) {
 			routerList.push({
-				data: temp[i] + conExt,
+				data: temp[i] + datExt,
 				cont: null
 			})
 		}
