ee593ec20bc4ae81260d2ebaedfc8dfadf7ce35c
mu2306 <185471581@qq.com>
2025-11-09 02:19:54 +0800

    更新和删除SQL语句参数支持动态传入

--- src/sql/remove.js
+++ src/sql/remove.js
@@ -11,17 +11,42 @@ module.exports = function (options, callback) {
 	// 检查是否使用逻辑删除
 	var useLogicalDelete = options.logical_delete !== false; // 默认为 true
 
-	var sqlCode;
+	// 构建 WHERE 子句：优先使用 options.query，如果没有则使用 options.data.id
+	var whereStr = '';
 	var params = [];
+	
+	if (options.query && Object.keys(options.query).length > 0) {
+		// 使用 options.query 构建 WHERE 条件
+		var whereConditions = [];
+		for (var queryKey in options.query) {
+			var queryValue = options.query[queryKey];
+			if (queryValue === null || queryValue === undefined) {
+				whereConditions.push('`' + queryKey + '` IS NULL');
+			} else {
+				whereConditions.push('`' + queryKey + '` = ?');
+				params.push(queryValue);
+			}
+		}
+		whereStr = 'WHERE ' + whereConditions.join(' AND ');
+	} else if (options.data && options.data.id) {
+		// 备用方案：使用 options.data.id
+		whereStr = 'WHERE `id` = ?';
+		params = [options.data.id];
+	} else {
+		// 如果没有 WHERE 条件，返回错误
+		if (callback && typeof callback === 'function') {
+			callback(new Error('DELETE 操作必须提供 WHERE 条件（options.query 或 options.data.id）'), null);
+		}
+		return;
+	}
 
+	var sqlCode;
 	if (useLogicalDelete) {
 		// 逻辑删除：更新 is_deleted 和 deleted_at 字段
-		sqlCode = 'UPDATE ' + options.tablename + ' SET is_deleted = 1, deleted_at = NOW() WHERE ID = ?';
-		params = [options.query.id];
+		sqlCode = 'UPDATE `' + options.tablename + '` SET `is_deleted` = 1, `deleted_at` = NOW() ' + whereStr;
 	} else {
 		// 物理删除：直接删除记录
-		sqlCode = 'DELETE FROM ' + options.tablename + ' WHERE ID = ?';
-		params = [options.query.id];
+		sqlCode = 'DELETE FROM `' + options.tablename + '` ' + whereStr;
 	}
 
 	db.query(sqlCode, params, function (err, data) {
--- src/sql/update.js
+++ src/sql/update.js
@@ -7,21 +7,53 @@ module.exports = function (options, callback) {
 
 	var db = conn(options.dataname);
 
-	var keys = Object.keys(options.data).join(', ');
-
-	var values = '"' + Object.values(options.data).join('","') + '"';
-
 	var updStr = '';
 
+	// 构建 SET 子句：排除 id 字段（id 不参与更新，只用于 WHERE 条件）
 	for (var key in options.data) {
-		if (key !== 'id') updStr += '' + key + '' + '=' + '"' + options.data[key] + '", ';
+		if (key !== 'id') {
+			// 处理 NULL 值
+			if (options.data[key] === null || options.data[key] === undefined) {
+				updStr += '`' + key + '` = NULL, ';
+			} else {
+				// 转义单引号，防止 SQL 注入
+				var value = String(options.data[key]).replace(/'/g, "''");
+				updStr += '`' + key + '` = ' + "'" + value + "', ";
+			}
+		}
 	}
 
 	updStr = updStr.replace(/, $/, ' ');
 
-	updStr += 'WHERE ID = ' + '"' + options.data.id + '"';
+	// 构建 WHERE 子句：优先使用 options.query，如果没有则使用 options.data.id
+	var whereStr = '';
+	if (options.query && Object.keys(options.query).length > 0) {
+		// 使用 options.query 构建 WHERE 条件
+		var whereConditions = [];
+		for (var queryKey in options.query) {
+			var queryValue = options.query[queryKey];
+			if (queryValue === null || queryValue === undefined) {
+				whereConditions.push('`' + queryKey + '` IS NULL');
+			} else {
+				// 转义单引号，防止 SQL 注入
+				var escapedValue = String(queryValue).replace(/'/g, "''");
+				whereConditions.push('`' + queryKey + '` = ' + "'" + escapedValue + "'");
+			}
+		}
+		whereStr = 'WHERE ' + whereConditions.join(' AND ');
+	} else if (options.data.id) {
+		// 备用方案：使用 options.data.id
+		var idValue = String(options.data.id).replace(/'/g, "''");
+		whereStr = 'WHERE `id` = ' + "'" + idValue + "'";
+	} else {
+		// 如果没有 WHERE 条件，返回错误
+		if (callback && typeof callback === 'function') {
+			callback(new Error('UPDATE 操作必须提供 WHERE 条件（options.query 或 options.data.id）'), null);
+		}
+		return;
+	}
 
-	var sqlCode = 'UPDATE ' + options.tablename + ' SET ' + updStr;
+	var sqlCode = 'UPDATE `' + options.tablename + '` SET ' + updStr + ' ' + whereStr;
 
 	db.query(sqlCode, function (err, data) {
 
