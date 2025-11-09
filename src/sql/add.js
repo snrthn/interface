fd30ed5a675afa35b65cb59defcace4db139016e
mu2306 <185471581@qq.com>
2025-11-09 21:37:11 +0800

    新增数据库SQL语句优化

--- src/sql/add.js
+++ src/sql/add.js
@@ -9,9 +9,24 @@ module.exports = function (options, callback) {
 
 	var keys = Object.keys(options.data).join(', ');
 
-	var values = '"' + Object.values(options.data).join('","') + '"';
+	// 正确处理所有数据类型，避免 [object Object] 问题
+	var values = [];
+	for (var key in options.data) {
+		var value = options.data[key];
+		// 处理 NULL 值
+		if (value === null || value === undefined) {
+			values.push('NULL');
+		} else {
+			// 确保值是基本类型（字符串、数字、布尔值）
+			var stringValue = String(value);
+			// 转义单引号，防止 SQL 注入
+			stringValue = stringValue.replace(/'/g, "''");
+			values.push("'" + stringValue + "'");
+		}
+	}
+	var valuesStr = values.join(', ');
 
-	var sqlCode = 'INSERT INTO ' + options.tablename + ' (' + keys + ') VALUES (' + values + ')';
+	var sqlCode = 'INSERT INTO `' + options.tablename + '` (' + keys + ') VALUES (' + valuesStr + ')';
 
 	db.query(sqlCode, function (err, data) {
 
